f45ce81baf297153124fd4d3b2d9a254c9b65e70
ONSISU <kmi5063@naver.com>
2025-10-21 17:36:13 +0900

    test

---
+++ src/test/java/com/example/hotel_back/HotelBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotel_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
